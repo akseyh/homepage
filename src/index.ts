7d400c0564ce3c94e27c4736bf11aa03986007e4
akseyh <yilmazsems@gmail.com>
2024-01-18 18:31:25 +0300

    update port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import path from "path";
 import matter from "gray-matter";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
